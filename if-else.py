2d431c0c7648498f04ed48edd7200d9b98a7e142
alexanderalban <albanalexander@gmail.com>
2022-06-01 16:35:53 -0500

    basic statement added

---
+++ if-else.py
@@ -0,0 +1,7 @@
+# If statements are used to set conditions for returns - If this, then that
+
+iron_man_suits = 25
+if iron_man_suits > 20:
+    print("That's too many suits!")
+    print("We talked about this!")
+    print("I want one!")
